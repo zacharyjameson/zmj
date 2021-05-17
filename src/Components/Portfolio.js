8ced97dd8b9a8a56d5d362c54a5591366c6514d8
Murphy <zacharymjameson@gmail.com>
2021-05-17 08:52:11 -0400

    portfolio animation progress

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -77,119 +77,128 @@ class Portfolio extends Component {
           </li>
           <li className="portfolioitem">
             <h5>Weathertop</h5>
-            <a
-              href="https://zacharyjameson.github.io/weathertop"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img
-                src={weathertop}
-                width="200"
-                alt="screenshot of weathertop app with san francisco weather results"
-              />
-            </a>
-            <p>
-              Simple web application to find the current weather in a queried
-              city. Allows users to return data in imperial or metric systems;
-              but defaults to kelvin if no option is selected.
-            </p>
-            <p>
-              Repo:
-              <a
-                href="https://github.com/zacharyjameson/weathertop"
-                target="_blank"
-                rel="noreferrer"
-              >
-                github.com/zacharyjameson/weathertop
-              </a>
-            </p>
-            <p>
-              Live App:
+            <div className="overlayContainer">
               <a
                 href="https://zacharyjameson.github.io/weathertop"
                 target="_blank"
                 rel="noreferrer"
               >
-                zacharyjameson.github.io/weathertop
+                <img
+                  src={weathertop}
+                  width="200"
+                  alt="screenshot of weathertop app with san francisco weather results"
+                />
               </a>
-            </p>
-            <p>Skills Used: HTML, CSS, JavaScript, React, GH Pages and JSX</p>
+              <div className="description">
+                Simple web application to find the current weather in a queried
+                city. Allows users to return data in imperial or metric systems;
+                but defaults to kelvin if no option is selected.
+              </div>
+              <div className="projectlinks">
+                <p>
+                  Repos <br />
+                  <a
+                    href="https://github.com/zacharyjameson/weathertop"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Client
+                  </a>{" "}
+                  <br />
+                </p>
+                <p>
+                  Demo <br />
+                  <a
+                    href="https://zacharyjameson.github.io/weathertop"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Weathertop
+                  </a>
+                </p>
+              </div>
+            </div>
           </li>
           <li className="portfolioitem">
             <h5>Musiciandex</h5>
-            <a
-              href="https://zacharyjameson.github.io/musiciandex/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img
-                src={musiciandex}
-                width="200"
-                alt="screenshot of musiciandex homepage"
-              />
-            </a>
-            <p>
-              I wanted to build a place where users could both check in on
-              upcoming events as well as get information on background and most
-              listened to albums of users' favorite artists as well as a tool to
-              get a snapshot view of artists they haven't listened to yet. Users
-              are able to enter in a band, artist or musician name and
-              Musiciandex will return the respective description of the input,
-              upcoming events and top 3 albums of the respective artist.
-              Refactoring for React utilization coming soon!
-            </p>
-            <p>
-              Repo:
-              <a
-                href="https://github.com/zacharyjameson/musiciandex"
-                target="_blank"
-                rel="noreferrer"
-              >
-                github.com/zacharyjameson/musiciandex
-              </a>
-            </p>
-            <p>
-              Live App:
+            <div className="overlayContainer">
               <a
                 href="https://zacharyjameson.github.io/musiciandex/"
                 target="_blank"
                 rel="noreferrer"
               >
-                zacharyjameson.github.io/musiciandex/
+                <img
+                  src={musiciandex}
+                  width="200"
+                  alt="screenshot of musiciandex homepage"
+                />
               </a>
-            </p>
-            <p>Skills Used: HTML, CSS, JavaScript, and jQuery</p>
+              <div className="description">
+                I wanted to build a place where users could both check in on
+                upcoming events as well as get information on background and
+                most listened to albums of users' favorite artists as well as a
+                tool to get a snapshot view of artists they haven't listened to
+                yet. Users are able to enter in a band, artist or musician name
+                and Musiciandex will return the respective description of the
+                input, upcoming events and top 3 albums of the respective
+                artist. Refactoring for React utilization coming soon!
+              </div>
+              <div className="projectlinks">
+                <p>
+                  Repos <br />
+                  <a
+                    href="https://github.com/zacharyjameson/musiciandex"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Client
+                  </a>
+                </p>
+                <p>
+                  Demo <br />
+                  <a
+                    href="https://zacharyjameson.github.io/musiciandex/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Musiciandex
+                  </a>
+                </p>
+              </div>
+            </div>
           </li>
           <div className="portfoliogroup">
             <li className="portfolioitem">
               <h5>One Quiz to Rule Them All</h5>
-              <a href="https://zacharyjameson.github.io/quiz-app/">
-                <img
-                  src={quizapp}
-                  width="200"
-                  alt="screen shot of quiz app question"
-                />
-              </a>
-              <p>
-                A meme-based quiz app going over the trials, tribulations and
-                overall tomfoolery in Middle Earth. Built using HTML, CSS,
-                JavaScript, and jQuery.
-              </p>
-              <p>
-                Repo:
-                <a href="https://github.com/zacharyjameson/quiz-app">
-                  github.com/zacharyjameson/quiz-app
-                </a>
-              </p>
-              <p>
-                Live App:
+              <div className="overlayContainer">
                 <a href="https://zacharyjameson.github.io/quiz-app/">
-                  zacharyjameson.github.io/quiz-app/
+                  <img
+                    src={quizapp}
+                    width="200"
+                    alt="screen shot of quiz app question"
+                  />
                 </a>
-              </p>
-              <p>Skills Used: HTML, CSS, JavaScript, and jQuery</p>
+                <div className="description">
+                  A meme-based quiz app going over the trials, tribulations and
+                  overall tomfoolery in Middle Earth. Built using HTML, CSS,
+                  JavaScript, and jQuery.
+                </div>
+                <div className="projectlinks">
+                  <p>
+                    Repos <br />
+                    <a href="https://github.com/zacharyjameson/quiz-app">
+                      Client
+                    </a>
+                  </p>
+                  <p>
+                    Demo <br />
+                    <a href="https://zacharyjameson.github.io/quiz-app/">
+                      Quiz
+                    </a>
+                  </p>
+                </div>
+              </div>
             </li>
-
             <li className="portfolioitem">
               <h5>Noteful</h5>
               <a
