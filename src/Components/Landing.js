d10c4fd1fac56a54cae155f987e7732c0c78c5e1
Murphy <zacharymjameson@gmail.com>
2022-10-29 16:07:52 -0400

    landing page change

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -7,7 +7,7 @@ class Landing extends Component {
     return (
      <div className="landing">
         <h1>Hi, I'm Zack Jameson.</h1>
-        <h1>I'm a full-stack web developer.</h1>
+        <h1>I'm a Cloud Engineer.</h1>
         <Link to="/portfolio">
           <button>View My Work</button>
         </Link>
