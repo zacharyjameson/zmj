c83d028b2ac4911a351ba1f0fb0c0d4a2855dc67
Murphy <zacharymjameson@gmail.com>
2023-04-11 08:16:23 -0400

    update career page

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -2,16 +2,15 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class Landing extends Component {
-
   render() {
     return (
-     <div className="landing">
+      <div className="landing">
         <h1>Hi, I'm Zack Jameson.</h1>
         <h1>I'm a Cloud Engineer.</h1>
         <Link to="/portfolio">
           <button>View My Work</button>
         </Link>
-        </div>
+      </div>
     );
   }
 }
